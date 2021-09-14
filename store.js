37a65c57052128c3de051910bf7ece2ff194ec10
HIR2009 <70085501+HIR2009@users.noreply.github.com>
2021-09-14 08:29:44 +0530

    Add files via upload

---
+++ store.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("slidingNav").style.width = "20%"
+  }
+  
+  function closeNav() {
+    document.getElementById("slidingNav").style.width = "0"
+  }
+
+  function myFunction(){
+    window.location.href = "https://hir2009.github.io/cart/";
+}
